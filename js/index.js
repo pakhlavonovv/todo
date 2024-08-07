9c6fd9fea5a569e189cf166b1b6bbfe2dfd71f1d
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2024-08-07 22:27:46 +0500

    todo

---
+++ js/index.js
@@ -0,0 +1,17 @@
+// Task 1
+
+// const createBtn = document.getElementById("createBtn")
+// const container = document.getElementById("container")
+// createBtn.addEventListener("click", function(){
+//     const counter_div = document.createElement("div")
+//     const plus_btn = document.createElement("button")
+//     const minus_btn = document.createElement("button")
+//     const total_display = document.createElement("span")
+//     let total = 0
+//     plus_btn.textContent = '+'
+//     minus_btn.textContent = '-'
+//     plus_btn.onclick = () => total_display.textContent = ++total
+//     minus_btn.onclick = () => total_display.textContent = --total
+//     counter_div.append(minus_btn, total_display, plus_btn)
+//     container.appendChild(counter_div)
+// })  
